55f9dd4a8a2994b45362e2993e8cd98ee181d0cd
Luki riski <contactlukiriski@gmail.com>
2024-01-04 00:29:37 +0700

    menambah fitur autentikasi

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,9 +1,64 @@
+import 'dart:convert';
+
+import 'package:employemanager/pages/homepage.dart';
 import 'package:employemanager/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+TextEditingController emailController = TextEditingController(); 
+TextEditingController passwordController = TextEditingController();
+
+void login(BuildContext context,String email, password) async{
+  try{
+    Response response =  await post(
+      Uri.parse('https://reqres.in/api/register'),
+      body: {
+        'email': email, 
+        'password': password,
+      }
+    );
+      if (!context.mounted) return;
+    if (response.statusCode == 200){
+     // Berhasil
+      Map<String, dynamic> responseData = json.decode(response.body);
+      if (responseData.containsKey('token')) {
+        Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) => const HomePage()),
+                (Route<dynamic> route) => false,
+              );
+      } else {
+       _showSnackBar(context, "Email atau Password Salah");
+      }
+
+    }else{
+           _showSnackBar(context, "Email atau Password Salah");
+    }
+  } catch(e){
+    e.toString();
+  }
+}
+
+void _showSnackBar(BuildContext context, String message) {
+  final snackBar = SnackBar(
+    content: Text(message),
+    backgroundColor: Colors.red,
+  );
+
+  
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +70,10 @@ class LoginPage extends StatelessWidget {
           children: [
              Text("EMPLOYEE MANAGER", style:primaryTextColor.copyWith(fontSize: 20, fontWeight:bold)),
 
+            //Form Input Email
              const SizedBox(height: 20,),
             TextField(
+              controller: emailController,
               decoration: InputDecoration(
                 hintText: "Email Addres",
                 border: OutlineInputBorder(
@@ -31,7 +88,10 @@ class LoginPage extends StatelessWidget {
             const SizedBox(
               height: 15,
             ),
+
+            //Form input password
             TextField(
+              controller: passwordController,
               decoration: InputDecoration(
                 hintText: "Password",
                 border: OutlineInputBorder(
@@ -48,15 +108,20 @@ class LoginPage extends StatelessWidget {
               height: 15,
             ),
 
-            Container(
-              height: 60, 
-              width: 300, 
-              decoration: BoxDecoration(
-                color: primaryColor, 
-                borderRadius: BorderRadius.circular(30)
-              ),
-              child: Center(
-                child: Text("Sign in", style:whiteTextStyle,),
+            GestureDetector(
+              onTap: () {
+                login(context,emailController.text.toString(), passwordController.text.toString());
+              },
+              child: Container(
+                height: 60, 
+                width: 300, 
+                decoration: BoxDecoration(
+                  color: primaryColor, 
+                  borderRadius: BorderRadius.circular(30)
+                ),
+                child: Center(
+                  child: Text("Sign In", style:whiteTextStyle,),
+                ),
               ),
             )
           ],
